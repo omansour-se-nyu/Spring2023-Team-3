51ba2a12e5566982116dbad98ab860959186d404
kirth123 <88990184+kirth123@users.noreply.github.com>
2023-03-06 01:07:40 -0500

    Delete register_patient.py

--- register_patient.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic
-
-class RegisterPatient(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('register_patient.ui', self)
