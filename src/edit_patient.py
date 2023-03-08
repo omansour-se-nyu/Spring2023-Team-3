7535902d7decbbf5b7272a1719e0e97717b9be9a
kirth123 <88990184+kirth123@users.noreply.github.com>
2023-03-08 17:00:13 -0500

    Delete src directory

--- src/edit_patient.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic
-
-class EditPatient(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('edit_patient.ui', self)
